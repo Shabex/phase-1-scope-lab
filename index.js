8edc15005949627cdda703dedd066900a714e085
Shabex <shabsibrah@gmail.com>
2022-06-07 22:51:02 +0300

    commit tree a070f78b2b60825ebbf007692e70b9fc7d21687e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(name){
+ customerName = customerName.toUpperCase()
+}
+
+var worstCustomer = "poor"
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+const  leastFavoriteCustomer = "jimmy"
+function changeLeastFavoriteCustomer(){
+   leastFavoriteCustomer = "Jane"
+}
